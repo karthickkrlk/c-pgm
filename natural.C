6563a8c8eec9347306557a29519ee4d1b405175a
karthickkrlk <31438455+karthickkrlk@users.noreply.github.com>
2018-01-17 16:18:16 +0530

    natural.C
    sum of natural numbers

---
+++ natural.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h.
+void main()
+{
+int n,a=0,c=0;
+printf("\n\n Enter number of times:");
+scanf("%d",&n);
+for(a=0;a<=n;a++)
+{
+c=c+a:
+}
+printf("\n the sum is:%d",&c);
+getch()
+}
